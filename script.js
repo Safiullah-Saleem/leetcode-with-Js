5e4a8274714f79bdf7b12db93a6c83f5c9256927
Safiullah-Saleem <safiullahsubadar@gmail.com>
2025-03-13 15:15:56 +0500

    Done

--- script.js
+++ script.js
@@ -17,21 +17,40 @@
 
 // other method to solve this problem
 
-let amount = Number(prompt("What is the final amount ?"));
-dis = 0;
+// let amount = Number(prompt("What is the final amount ?"));
+// dis = 0;
 
-if (isNaN(amount)) {
-  console.log("Wrong amount");
-} else if (amount > 0 && amount <= 5000) {
-  dis = 0;
-} else if (amount > 5001 && amount <= 7000) {
-  dis = 5;
-} else if (amount > 7001 && amount <= 9000) {
-  dis = 10;
-} else if (amount > 9000) {
-  dis = 20;
-} else {
-  console.log("Your pay able amount is not correct");
+// if (isNaN(amount)) {
+//   console.log("Wrong amount");
+// } else if (amount > 0 && amount <= 5000) {
+//   dis = 0;
+// } else if (amount > 5001 && amount <= 7000) {
+//   dis = 5;
+// } else if (amount > 7001 && amount <= 9000) {
+//   dis = 10;
+// } else if (amount > 9000) {
+//   dis = 20;
+// } else {
+//   console.log("Your pay able amount is not correct");
+// }
+
+// console.log(amount - Math.floor((dis * amount) / 100));
+
+// Electricity bill
+let unit = Number(prompt("Please Enter the total units of your bill"));
+amount = 0;
+if (unit > 400) {
+  amount = (unit - 400) * 13;
+  unit = 400;
 }
+if (unit > 200 && unit <= 400) {
+  amount += (unit - 200) * 8;
+  unit = 200;
+}
+if (unit > 100 && unit <= 200) {
+  amount += (unit - 100) * 6;
+  unit = 100;
+}
+amount += unit * 4;
 
-console.log(amount - Math.floor((dis * amount) / 100));
+console.log("Here is your actual amounnt after discount =>", amount);
